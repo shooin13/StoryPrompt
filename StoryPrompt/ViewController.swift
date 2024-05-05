bbcbc5f8e2bf7a2c675f65792ed2eed68c5eff5f
shooin13 <shooin13@gmail.com>
2024-05-05 19:59:46 +0300

    - added dismissing keyboard after texfiled entry - added functionaluty to Generate Story Prompt button

--- StoryPrompt/ViewController.swift
+++ StoryPrompt/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
   }
   
   @IBAction func changeStoryType(_ sender: UISegmentedControl) {
-    if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex) {
+     if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex) {
       storyPrompt.genre = genre
     } else {
       storyPrompt.genre = .scifi
@@ -26,18 +26,29 @@ class ViewController: UIViewController {
     print(storyPrompt.genre)
   }
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    numberSlider.value = 7.5
-    storyPrompt.noun = "toaster"
-    storyPrompt.adjective = "smelly"
-    storyPrompt.verb = "burps"
-    storyPrompt.number = Int(numberSlider.value)
+  @IBAction func generateStoryPrompt(_ sender: Any) {
+    updateStoryPrompt()
     print(storyPrompt)
-    
   }
   
   
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    numberSlider.value = 7.5
+  }
+  
+  private func updateStoryPrompt() {
+    storyPrompt.noun = nounTextField.text ?? ""
+    storyPrompt.adjective = adjectiveTextField.text ?? ""
+    storyPrompt.verb = verbTextField.text ?? ""
+  }
+  
 }
 
-
+extension ViewController: UITextFieldDelegate {
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    updateStoryPrompt()
+    return true
+  }
+}
